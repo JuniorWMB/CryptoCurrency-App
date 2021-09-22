4ccc7d66b84689bbbe475c4421f36f268bc0f948
Junior <juniormbaya243@gmail.com>
2021-09-22 20:08:12 +0200

    finish to build a structure of cryptodetails page

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -57,6 +57,83 @@ const genericStats = [
             {date}
         </Option>)}
           </Select>
+          {/* line chart */}
+          <Col className='stats-container'>
+              <Col className='coin-value-statistics'>
+                <Col className='coin-value-statistics-heading'>
+                    <Title level={3} className='coin-details-heading'>
+                    {cryptoDetails.name} Value Statistics
+                    </Title>
+                    <p>
+                        An overview showing th stats of {cryptoDetails.name}
+                    </p>
+                </Col>
+                {stats.map(({icon,title,value})=>
+                (
+                    <Col className='coin-stats'>
+                        <Col className="coin-stats-name">
+                            <Text>
+                                {icon}
+                            </Text>
+                            <Text>
+                                {title}
+                            </Text>
+                        </Col>
+                        <Text className='stats'>{value}</Text>
+                    </Col>
+                )
+                )}
+              </Col>
+
+              <Col className='other-stats-info'>
+                <Col className='coin-value-statistics-heading'>
+                    <Title level={3} className='coin-details-heading'>
+                   Other Statistics
+                    </Title>
+                    <p>
+                        An overview showing th stats of all cryptocurrencies
+                    </p>
+                </Col>
+                {genericStats.map(({icon,title,value})=>
+                (
+                    <Col className='coin-stats'>
+                        <Col className="coin-stats-name">
+                            <Text>
+                                {icon}
+                            </Text>
+                            <Text>
+                                {title}
+                            </Text>
+                        </Col>
+                        <Text className='stats'>{value}</Text>
+                    </Col>
+                )
+                )}
+              </Col>
+
+          </Col>
+          <Col className="coin-desc-link">
+              <Row className='coind-desc'>
+                    <Title level={3} className='coin-details-heading'>
+                        What is {cryptoDetails.name} ?
+                        {HTMLReactParser(cryptoDetails.description)}
+                    </Title>
+              </Row>
+              <Col className='coin-links'>
+                  <Title level={3} className="coin-details-heading"> {cryptoDetails.name} Links
+                  </Title>
+                  {cryptoDetails.links.map((link)=>(
+                      <Row className='coin-link' key={link.name}>
+                          <Title level={5} className='link-name'>
+                              {link.type}   
+                          </Title>
+                            <a href={link.url} target='_blank' rel='noreferrer'>
+                                {link.name}
+                            </a>
+                      </Row>
+                  ))}
+              </Col>
+          </Col>
         </Col>
     )
 }
