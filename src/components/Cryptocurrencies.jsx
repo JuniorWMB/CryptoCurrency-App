3b827ca02587183b0a5672caab1f1841e274ae51
Junior <juniormbaya243@gmail.com>
2021-09-22 17:52:59 +0200

    create a disapear input and finish homepage,cryptocurrencie page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -21,11 +21,13 @@ const Cryptocurrencies = ({simplified}) => {
     if(isFetching) return'Loading...'
     return (
        <>
+       {!simplified&&(
        <div className="search-crypto">
            <Input placeholder="Search Cryptocurrency" onChange={
                (e)=>setSearchTerm(e.target.value)
            }/>
        </div>
+       )}
        <Row gutter={[32,32]} className="crypto-card-container">
         {cryptos?.map((currency)=>(
             <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
