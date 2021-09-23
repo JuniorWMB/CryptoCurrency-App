5c8913bb4872f134a80ebfdd53148cff87e31ecc
Junior <juniormbaya243@gmail.com>
2021-09-23 21:06:08 +0200

    add rounded gif for loading moment

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,9 @@ import Loading from '../images/giphy.gif'
 const Loader = () => {
     return (
         <div className='loader'>
-            <img src={Loading} alt='loading...'/>
+            <img  src={Loading} alt='loading...' width='200px' height='200px' style={{
+                borderRadius:'50%'
+            }}/>
         </div>
     )
 }
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const cryptoNewsHeaders = {
   "x-bingapis-sdk": "true",
   "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
-  "x-rapidapi-key": "0cead4f09fmsheafb83c3e96fa06p19944ajsn5d82eb47b567",
+  "x-rapidapi-key": process.env.REACT_APP_CRYPTO_RAPIDAPI_KEY,
 };
 
 const baseUrl = "https://bing-news-search1.p.rapidapi.com";
